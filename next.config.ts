6cda721b1cd78d1a8637ae3959240d150036af3a
Dmytro Koval <contact@dkoval.pro>
2024-11-01 17:50:53 +0200

    Add initial content

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(js|ts)x?$/, // Apply only if the SVG is imported in JS/TS files
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
